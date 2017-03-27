d5b865bb02dec50b5c19b4aaeb19d6d1a3e8f061
stehnoc <chris.stehno@netscout.com>
2017-03-27 09:28:54 -0500

    started bob

---
+++ bob/src/lib.rs
@@ -0,0 +1,13 @@
+
+pub fn reply(input : String) -> String {
+    match input {
+        _ => "Whatever."
+    }
+}
+
+
+// Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+// Bob answers 'Sure.' if you ask him a question. (ends with ?)
+// He answers 'Whoa, chill out!' if you yell at him. (all caps)
+// He says 'Fine. Be that way!' if you address him without actually saying anything. (empty)
+// He answers 'Whatever.' to anything else.
