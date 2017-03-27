1445c3ac55918b0f3b3613c9b4ab14cc16ff0467
stehnoc <chris.stehno@netscout.com>
2017-03-27 08:45:06 -0500

    my raindrops solution

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,21 +1,20 @@
-use std::borrow::Cow;
 
-pub fn raindrops(number : u32 ) -> &'static str {
-    let mut result : String = String::from("");
+pub fn raindrops(number : u32 ) -> String {
+    let mut drops : String = String::from("");
 
     if number % 3 == 0 {
-        result.push_str("Pling");
+        drops.push_str("Pling");
     }
     if number % 5 == 0 {
-        result.push_str("Plang");
+        drops.push_str("Plang");
     }
     if number % 7 == 0 {
-        result.push_str("Plong");
+        drops.push_str("Plong");
     }
 
     if number % 3 != 0 && number % 5 != 0 && number % 7 != 0 {
-        result.push_str("1");
+        drops.push_str(&number.to_string());
     }
 
-    return &*result.as_str();
+    return drops;
 }
