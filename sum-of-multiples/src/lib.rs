a978047672a872707bcaa576bbe27404cd6b5100
stehnoc <chris.stehno@netscout.com>
2017-03-28 09:30:59 -0500

    first pass at sum-of-multiples

---
+++ sum-of-multiples/src/lib.rs
@@ -0,0 +1,16 @@
+use std::collections::BTreeSet;
+
+pub fn sum_of_multiples(number: u32, multiples: &Vec<u32>) -> u32 {
+    let mut numbers = BTreeSet::new();
+
+    for n in 1..number {
+        for m in multiples {
+            if n % m == 0 {
+                numbers.insert(n);
+            }
+        }
+    }
+
+    let sum = numbers.iter().fold(0, |sum,n| sum + n);
+    return sum;
+}
