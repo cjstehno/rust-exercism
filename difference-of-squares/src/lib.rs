8b6fcafffc41b5dc00bc1d66ee99dc46a52a4277
stehnoc <chris.stehno@netscout.com>
2017-03-28 08:52:23 -0500

    tighter code

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,18 +1,10 @@
 
 pub fn square_of_sum(number: u32) -> u32 {
-    let mut sum = 0;
-    for n in 1..number+1 {
-        sum += n;
-    }
-    return sum.pow(2);
+    return ((1..number+1).fold(0, |sum, x| sum + x)).pow(2);
 }
 
 pub fn sum_of_squares(number: u32) -> u32 {
-    let mut sum = 0;
-    for n in 1..number+1 {
-        sum += n.pow(2);
-    }
-    return sum;
+    return (1..number+1).fold(0, |sum, x| sum + x.pow(2));
 }
 
 pub fn difference(number: u32) -> u32 {
