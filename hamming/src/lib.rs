e269b2bc291aed61c804b141fa0468993b3409c1
stehnoc <chris.stehno@netscout.com>
2017-03-29 06:56:30 -0500

    revised hamming solution

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,10 +1,7 @@
 
 pub fn hamming_distance(first: &str, second: &str) -> Result<usize,()> {
-    if first.len() == second.len() {
-        let count: usize = first.chars().zip(second.chars()).filter(|cs| cs.0 != cs.1 ).count();
-        return Ok(count);
-
-    } else {
-        return Err(());
+    return match first.len() == second.len(){
+        true => Ok(first.chars().zip(second.chars()).filter(|cs| cs.0 != cs.1 ).count()),
+        false => Err(())
     }
 }
