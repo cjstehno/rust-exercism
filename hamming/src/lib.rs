7d44f5ed5e4a7299f4c5c40bc437fd10b55f3d23
stehnoc <chris.stehno@netscout.com>
2017-03-29 06:53:05 -0500

    first pass at hamming.

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,9 +1,9 @@
 
-pub fn hamming_distance(first: &String, second: &String) -> Result<usize,()> {
-    let count: usize = first.chars().zip(second.chars()).filter(|cs| cs.0 != cs.1 ).count();
-
-    if count > 0 {
+pub fn hamming_distance(first: &str, second: &str) -> Result<usize,()> {
+    if first.len() == second.len() {
+        let count: usize = first.chars().zip(second.chars()).filter(|cs| cs.0 != cs.1 ).count();
         return Ok(count);
+
     } else {
         return Err(());
     }
