d108693e6885791a3e8767893a94669f94b893a5
stehnoc <chris.stehno@netscout.com>
2017-03-29 11:23:55 -0500

    tight version of pangrams

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,12 +1,8 @@
 use std::collections::HashSet;
 
 pub fn is_pangram(sentence: &str) -> bool {
-    let mut letter_counts = HashSet::new();
-
-    for ch in sentence.to_uppercase().chars() {
-        letter_counts.insert(ch);
-    }
+    let found_letters = sentence.to_uppercase().chars().collect::<HashSet<char>>();
 
     let all_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect::<Vec<char>>();
-    return all_letters.iter().all(|letter| letter_counts.contains(letter));
+    return all_letters.iter().all(|letter| found_letters.contains(letter));
 }
