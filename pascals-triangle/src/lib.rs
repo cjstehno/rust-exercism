fe45943d5d567168fc34b5b60691b9e3d283bbcc
stehnoc <chris.stehno@netscout.com>
2017-03-29 08:32:14 -0500

    cleaned up pascals a bit more

--- pascals-triangle/src/lib.rs
+++ pascals-triangle/src/lib.rs
@@ -8,23 +8,18 @@ impl PascalsTriangle {
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        let mut holder : Vec<Vec<u32>> = vec![];
-
-        if *&self.row_count > 0 {
-            for r in 0..*&self.row_count {
-                holder.push( PascalsTriangle::row(r) );
-            }
+        return match *&self.row_count {
+            0 => vec![],
+            _ => (0..*&self.row_count).map(|r| PascalsTriangle::row(r)).collect::<Vec<Vec<u32>>>()
         }
-
-        return holder;
     }
 
     fn row(row_num: u32) -> Vec<u32> {
         let mut row : Vec<u32> = vec![1];
 
         for i in 0..row_num {
-            let other : u32 = row[i as usize];
-            row.push( other * (row_num - i) / (i + 1) );
+            let item : u32 = row[i as usize];
+            row.push( item * (row_num - i) / (i + 1) );
         }
 
         return row;
