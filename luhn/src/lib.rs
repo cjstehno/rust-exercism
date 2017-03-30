df8512bbab3bcb7ee27297a84a8cd793e7599b10
cjstehno <chris@stehno.com>
2017-03-30 13:47:31 -0500

    first pass at luhn

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,38 +1,44 @@
 
 pub fn is_valid(card_num: &str) -> bool {
-    if card_num.len() < 2 {
-        return false;
-    }
+    return is_number_valid(card_num) && luhn_checksum(card_num) == 0
+}
 
-    let mut numbers : Vec<u32> = card_num.chars().rev().filter(|c| !c.is_whitespace()).map(|c| c.to_digit(10).unwrap() ).collect::<Vec<u32>>();
+fn luhn_checksum(card_num: &str) -> u32 {
+    let digits : Vec<u32> = digits_of(card_num);
+    let mut even_digits : Vec<u32> = Vec::new();
+    let mut odd_digits : Vec<u32> = Vec::new();
 
-    let mut doubler = false;
-    for n in 0..numbers.len() {
-        if doubler {
-            let double : u32 = numbers[n] * 2;
-            println!("Double: {}" , double);
-            numbers[n] = match double {
-                0 => 0,
-                _ if double > 9 => sum_digits(double),
-                _ => double - 9
-            }
+    let mut evens = false;
+    for d in digits {
+        if evens {
+            even_digits.push(d);
+        } else {
+            odd_digits.push(d);
         }
 
-        numbers[n] = sum_digits(numbers[n]);
-
-        doubler = !doubler;
+        evens = !evens;
     }
 
-    let sum : u32 = numbers.iter().sum();
-    println!("Sum: {}", sum);
+    let mut sum: u32 = odd_digits.iter().sum();
 
-    let check: u32 = sum % 10;
-    println!("Mod: {}", check);
+    for d in even_digits {
+        sum += sum_digits(2 * d);
+    }
 
-    let result: bool = sum.to_string().chars().last().unwrap().to_digit(10).unwrap() == check;
-    return result;
+    return sum % 10;
+}
+
+fn digits_of(number: &str) -> Vec<u32> {
+    return number.chars().rev()
+        .filter(|c| !c.is_whitespace() && c.is_numeric() )
+        .map(|c| c.to_digit(10).unwrap() )
+        .collect::<Vec<u32>>();
 }
 
 fn sum_digits(number: u32) -> u32 {
     return number.to_string().chars().map(|x| x.to_digit(10).unwrap() ).sum();
+}
+
+fn is_number_valid(card_num: &str) -> bool {
+    return card_num.trim().len() > 1 && card_num.chars().filter(|x| !x.is_whitespace()).all(|x| x.is_numeric() );
 }
