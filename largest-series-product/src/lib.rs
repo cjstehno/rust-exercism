448104c2be73e1eca865f7ba54d4fcaaf0729f23
cjstehno <chris@stehno.com>
2017-03-31 12:51:48 -0500

    tighter lsp solution

--- largest-series-product/src/lib.rs
+++ largest-series-product/src/lib.rs
@@ -1,4 +1,4 @@
-
+use std::cmp::max;
 
 pub fn lsp(digit_string: &str, count: usize) -> Result<u32,()> {
     if count > digit_string.len() || digit_string.chars().any(|c| !c.is_numeric()) {
@@ -9,12 +9,12 @@ pub fn lsp(digit_string: &str, count: usize) -> Result<u32,()> {
         return Ok(1);
     }
 
-    let mut products : Vec<u32> = vec![];
+    let mut max_product : u32 = 0;
     for series in digit_string.chars().map(to_digit).collect::<Vec<u32>>().windows(count) {
-        products.push( series.iter().product() );
+        max_product = max( max_product, series.iter().product() );
     }
 
-    return Ok(*products.iter().max().unwrap());
+    return Ok(max_product);
 }
 
 fn to_digit(ch : char) -> u32 {
