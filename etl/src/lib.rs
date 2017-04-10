55c865000a3d39a0df79f0cb10e638be47ebaec6
stehnoc <chris.stehno@netscout.com>
2017-04-10 11:51:42 -0500

    pass at etl

---
+++ etl/src/lib.rs
@@ -0,0 +1,13 @@
+use std::collections::BTreeMap;
+
+pub fn transform( input : &BTreeMap<i32, Vec<String>> ) -> BTreeMap<String, i32> {
+    let mut output : BTreeMap<String,i32> = BTreeMap::new();
+
+    for (id, words) in input.iter() {
+        for word in words.iter() {
+            output.insert( word.to_lowercase(), *id );
+        }
+    }
+
+    return output;
+}
