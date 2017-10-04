2aab84db0c608017c51b7042fdaf0e3b8d04cbf9
stehnoc <chris.stehno@netscout.com>
2017-10-04 09:48:05 -0500

    Updated to remove return

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -6,5 +6,5 @@ use chrono::Duration;
 
 /// Calculates the `DateTime<UTC>` value for 1 Gigasecond (10^9 s) after the provided `start_date`.
 pub fn after(start_date: DateTime<UTC>) -> DateTime<UTC> {
-    return start_date + Duration::seconds(1_000_000_000i64);
+    start_date + Duration::seconds(1_000_000_000i64)
 }
