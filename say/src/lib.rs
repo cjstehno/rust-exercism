d4f40e14dd204e3efc70c21affeb8f3929b14f8d
cjstehno <chris@stehno.com>
2017-10-05 14:37:58 -0500

    initial pass at say

--- say/src/lib.rs
+++ say/src/lib.rs
@@ -1,62 +1,78 @@
+const NUMBER_NAMES: [&str; 20] = [
+    "", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine", " ten", " eleven",
+    " twelve", " thirteen", " fourteen", " fifteen", " sixteen", " seventeen", " eighteen", " nineteen"
+];
+
+const TENS_NAMES: [&str; 10] = [
+    "", " ten", " twenty", " thirty", " forty", " fifty", " sixty", " seventy", " eighty", " ninety"
+];
+
 pub fn encode(number: u64) -> String {
-    match number {
-        _ if number < 10 => single_digits(number),
-        _ if number < 20 => teens(number),
-        // TODO: more in here
-        20 => String::from("twenty"),
-        21 => String::from("twenty-one"),
-        22 => String::from("twenty-two"),
-        _ if number < 1000 => hundreds(number),
-        _ => panic!("Unsupported number value")
+    if number == 0 {
+        return String::from("zero");
     }
+
+    let mut words: String = String::new();
+    let mut current = number;
+
+    if current > 999_999_999_999_999_999 {
+        words.push_str(&format!("{} quintillion ", encode_part(current / 1_000_000_000_000_000_000)));
+        current = current % 1_000_000_000_000_000_000;
+    }
+
+    if current > 999_999_999_999_999 {
+        words.push_str(&format!("{} quadrillion ", encode_part(current / 1_000_000_000_000_000)));
+        current = current % 1_000_000_000_000_000;
+    }
+
+    if current > 999_999_999_999 {
+        words.push_str(&format!("{} trillion ", encode_part(current / 1_000_000_000_000)));
+        current = current % 1_000_000_000_000;
+    }
+
+    if current > 999_999_999 {
+        words.push_str(&format!("{} billion ", encode_part(current / 1_000_000_000)));
+        current = current % 1_000_000_000;
+    }
+
+    if current > 999_999 {
+        words.push_str(&format!("{} million ", encode_part(current / 1_000_000)));
+        current = current % 1_000_000;
+    }
+
+    if current > 999 {
+        words.push_str(&format!("{} thousand ", encode_part(current / 1_000)));
+        current = current % 1_000;
+    }
+
+    words.push_str(encode_part(current).trim());
+
+    return words.trim().to_string();
 }
 
-fn single_digits(number: u64) -> String {
-    match number {
-        0 => String::from("zero"),
-        1 => String::from("one"),
-        2 => String::from("two"),
-        3 => String::from("three"),
-        4 => String::from("four"),
-        5 => String::from("five"),
-        6 => String::from("six"),
-        7 => String::from("seven"),
-        8 => String::from("eight"),
-        9 => String::from("nine"),
-        _ => panic!("invalid number")
+fn encode_part(number: u64) -> String {
+    let mut words: String;
+
+    let mut current = number;
+
+    if current % 100 < 20 {
+        words = NUMBER_NAMES[(current % 100) as usize].to_string();
+        current /= 100;
+    } else {
+        words = NUMBER_NAMES[(current % 10) as usize].to_string();
+        current /= 10;
+
+        if words.is_empty() {
+            words = format!("{}{}", TENS_NAMES[(current % 10) as usize], words.trim());
+        } else {
+            words = format!("{}-{}", TENS_NAMES[(current % 10) as usize], words.trim());
+        }
+        current /= 10;
     }
-}
 
-fn teens(number: u64) -> String {
-    match number {
-        10 => String::from("ten"),
-        11 => String::from("eleven"),
-        12 => String::from("twelve"),
-        13 => String::from("thirteen"),
-        14 => String::from("fourteen"),
-        15 => String::from("fifteen"),
-        16 => String::from("sixteen"),
-        17 => String::from("seventeen"),
-        18 => String::from("eighteen"),
-        19 => String::from("nineteen"),
-        _ => panic!("invalid number")
+    if current == 0 {
+        return words.trim().to_string();
     }
+
+    return format!("{} hundred{}", NUMBER_NAMES[current as usize], words).trim().to_string();
 }
-
-fn hundreds(number: u64) -> String {
-    match number {
-
-    }
-}
-
-/*
-
-    0 - 9 : single_digits
-    10 - 19 : teens
-    20 - 29 : twenties
-    ... thirties, fourties, fifties, ...
-    100 ...
-    1000
-
-    encode ( n / 100)
-*/
