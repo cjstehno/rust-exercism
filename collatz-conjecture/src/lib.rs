da491d077da31d4bc6d4f9e86b09a1279e96dfc1
cjstehno <chris@stehno.com>
2017-10-20 08:29:11 -0500

    removed redundant comments

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -7,10 +7,8 @@ pub fn collatz(mut n: u64) -> Result<u64, &'static str> {
 
         while n != 1 {
             if n % 2 == 0 {
-                // divide n by 2 to get n / 2
                 n = n / 2;
             } else {
-                // multiply n by 3 and add 1 to get 3n + 1
                 n = (n * 3) + 1;
             }
             steps += 1;
