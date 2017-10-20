5c45b74d9ad6f0877c964172250413ef244dfc6d
cjstehno <chris@stehno.com>
2017-10-20 08:28:30 -0500

    solution for conjecture

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,4 +1,21 @@
 // return Ok(x) where x is the number of steps required to reach 1
-pub fn collatz(n: u64) -> Result<u64, &'static str> {
+pub fn collatz(mut n: u64) -> Result<u64, &'static str> {
+    if n == 0 {
+        return Err("invalid input");
+    } else {
+        let mut steps: u64 = 0;
 
+        while n != 1 {
+            if n % 2 == 0 {
+                // divide n by 2 to get n / 2
+                n = n / 2;
+            } else {
+                // multiply n by 3 and add 1 to get 3n + 1
+                n = (n * 3) + 1;
+            }
+            steps += 1;
+        }
+
+        Ok(steps)
+    }
 }
