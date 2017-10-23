bfcf5eec3b22e0654fba588e90efd5dd8641db83
cjstehno <chris@stehno.com>
2017-10-23 09:16:58 -0500

    working perfect numbers

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -1,8 +1,28 @@
-pub fn classify(number: u32) -> Result<Classification, &'static str> {
+pub fn classify(number: u64) -> Result<Classification, &'static str> {
+    if number < 1 {
+        return Err("Number must be positive");
+    }
 
-
+    match calculate_aliquot(number) {
+        a if a == number => Ok(Classification::Perfect),
+        a if a > number => Ok(Classification::Abundant),
+        _ => Ok(Classification::Deficient)
+    }
 }
 
+fn calculate_aliquot(number: u64) -> u64 {
+    let mut sum = 0;
+
+    for n in 1..number {
+        if number % n == 0 {
+            sum += n;
+        }
+    }
+
+    sum
+}
+
+#[derive(Debug)] #[derive(PartialEq)]
 pub enum Classification {
     Perfect,
     Abundant,
